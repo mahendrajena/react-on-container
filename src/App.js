f204922a451e93b7a913503a23aa5bbe2caecea9
jmahen <mahendrakumar.jena@gmail.com>
2021-08-17 14:04:34 +0000

    Added new file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sample React App on Container
+          Sample React App on Container 2
         </p>
         
       </header>
