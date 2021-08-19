cc2d5da47966d3ea0177d99f8b521129586755ff
jmahen <30362646+jmahen@users.noreply.github.com>
2021-08-19 10:20:05 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sample React App on Container
+          Sample React App on Container (TeamCityBuild)
         </p>
         <div>My Env : {process.env.NODE_ENV}</div>
       </header>
